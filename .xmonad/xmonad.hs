4db85457a3d2eab4e6453e18d6e4f6d7bb592806
Warrick Lo <warrick.s.z.lo@gmail.com>
2021-01-17 17:45:29 -0800

    Display current XMonad layout inside Polybar
    Adding DynamicLog and DBus to the XMonad configuration to output the
    current tiling layout inside of a Polybar script module. xmonad-log
    is required to run properly.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -25,7 +25,10 @@ import XMonad.Layout.ToggleLayouts ( ToggleLayout(..), toggleLayouts )
 import XMonad.Util.Run ( safeSpawn, safeSpawnProg )
 import XMonad.Util.SpawnOnce
 
+import qualified Codec.Binary.UTF8.String as UTF8
 import qualified Data.Map as M
+import qualified DBus as D
+import qualified DBus.Client as D
 
 import qualified XMonad.StackSet as W
 
@@ -226,10 +229,36 @@ manageHook =
         , className =? "Gimp" --> doFloat -- Gimp windows.
         ]
 
+-- Log hook
+
+-- Emit a DBus signal on log updates.
+dbusOutput :: D.Client -> String -> IO ()
+dbusOutput dbus str = do
+    let signal = (D.signal objectPath interfaceName memberName) {
+        D.signalBody = [D.toVariant $ UTF8.decodeString str]
+    }
+    D.emit dbus signal
+  where
+    objectPath = D.objectPath_ "/org/xmonad/Log"
+    interfaceName = D.interfaceName_ "org.xmonad.Log"
+    memberName = D.memberName_ "Update"
+
+-- Dynamic log with pretty printing format, outputted to DBus.
+dbusLogHook :: D.Client -> PP
+dbusLogHook dbus = def
+    { ppOutput = dbusOutput dbus
+    , ppOrder = \(_:l:_:_) -> [l]  -- Only output the layout.
+    }
+
 -- Main
 
 main :: IO ()
 main = do
+    dbus <- D.connectSession
+    -- Request access to the DBus name.
+    D.requestName dbus (D.busName_ "org.xmonad.Log")
+        [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+
     xmonad $ ewmh $ docks $ withNavigation2DConfig navigation2DConfig $ def
         { XMonad.modMask = Main.modMask
         , XMonad.workspaces = Main.workspaces
@@ -247,5 +276,6 @@ main = do
         , XMonad.layoutHook = Main.layoutHook
         , XMonad.startupHook = Main.startupHook
         , XMonad.manageHook = Main.manageHook
+        , logHook = dynamicLogWithPP (dbusLogHook dbus)
         , handleEventHook = fullscreenEventHook <+> ewmhDesktopsEventHook
         }
